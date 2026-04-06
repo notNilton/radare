f4ab7e403b6b6c23c820d400986ae5a005aaa955
notNilton <notNilton@thor>
2026-04-06 10:30:10 -0400

    feat(frontend): configurar rotas protegidas e navegação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.scss";
 import "./styles/Global.css";
-import Node from "./components/Canva/Node";
-import NavbarComponent from "./components/Navbar/NavbarComponent";
-import AboutModal from "./components/About/AboutModal";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
 
 const App: React.FC = () => {
-  const [showAbout, setShowAbout] = useState<boolean>(false);
-
-  const toggleAboutPopup = () => {
-    setShowAbout(!showAbout);
-  };
-
   return (
-    <div className="app-container">
-      <NavbarComponent version={""} />
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<LoginPage />} />
 
-      <Node />
-      <AboutModal showAbout={showAbout} toggleAboutPopup={toggleAboutPopup} />
-    </div>
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<HomePage />} />
+          {/* Add more protected routes here as needed */}
+        </Route>
+
+        {/* Catch-all Route: Redirect to Home if authenticated, or Login if not */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
